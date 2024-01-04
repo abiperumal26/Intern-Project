4103ca490d9fc100805f1dee46af8c1926b89a50
Abinaya <email2abiperumal@gmail.com>
2024-01-04 07:28:46 -0330

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,36 @@
+// Home.js
+import React from 'react';
+import House from './House1.png';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="container">
+            <div className="nav">
+                <div className="logo">LUGAR</div>
+                <ul>
+                    <li>HOME</li>
+                    <li>ABOUT</li>
+                    <li>PROJECT</li>
+                    <li>CONTACT</li>
+                </ul>
+            </div>
+            <div className="content">
+                <div className="textContent">
+                    <h1>A home is 
+                        <br />
+                        built with love <br />
+                        and dreams</h1>
+                        <p>Real estate farm that makes your dreams true</p>
+                    <div className="buttonContainer">
+                        <button className="button">Our Projects</button>
+                        <button className="button1">Contact Us</button>
+                    </div>
+                </div>
+                <img src={House} alt="House" />
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/Page2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Page2.css';
+import left from './left bg.jpg';
+
+const Page2 = () => {
+    return (   
+        <div>
+            <img src={left} alt="Second" />
+            <h1>Award winning real <br />estate company in Dubai</h1>
+  <p>Semper arcu mauris aliquam lacus. Massa erat vitae ultrices pharetra scelerisque. Ipsum, turpis facilisis tempor pulvinar in lobortis ornare magna.</p>    
+        </div> 
+    );
+}
+
+export default Page2;
